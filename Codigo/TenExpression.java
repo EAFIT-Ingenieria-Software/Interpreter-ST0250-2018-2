b1f54595fdee90eb087d8542b9d8fd7566db8019
ksgomezp <ksgomezp@eafit.edu.co>
2018-11-12 11:21:17 -0500

    Add files via upload

---
+++ Codigo/TenExpression.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interpreter;
+
+/**
+ *
+ * @author ksgomezp
+ */
+public class TenExpression extends Expression { 
+	public String one() {
+      return "X";
+    }
+    public String four() {
+      return "XL";
+    }
+    public String five() {
+      return "L";
+    }
+    public String nine() {
+      return "XC";
+    }
+    public int multiplier() {
+      return 10;
+    }
+}
+
